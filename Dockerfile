ca972b4e81b997a012aa2c935fe06b0b401fa1d8
Wilinton <wilintongabrielss1@gmail.com>
2025-07-27 15:27:59 -0300

    config: atualiza versão do go no container e adiciona gin ao projeto

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
-FROM golang:1.22
+FROM golang:1.23
 
 WORKDIR /app
 
 COPY . .
+
+EXPOSE 8000
 
 CMD ["go", "run", "."]
--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("deu certo")
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"menssagem": "Servidor funcionando!",
+		})
+	})
+
+	r.Run(":8000")
 }
