e9320cbf109b5de77aa34d25beb07b802d05f183
Wilinton <wilintongabrielss1@gmail.com>
2025-07-28 20:42:05 -0300

    feat: adiciona suporte a cors e altera tipo do endpoint para interagir com o front

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/willGabriell/api-go-satoshis/internal/handler"
 )
@@ -8,9 +11,19 @@ import (
 func main() {
 	r := gin.Default()
 
+	// Configuração CORS
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:8080", "http://localhost:3000", "http://127.0.0.1:8080"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	convertHandler := handler.NewConvertHandler()
 
-	r.GET("/converter", convertHandler.HandleConverter)
+	r.POST("/converter", convertHandler.HandleConverter)
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"menssagem": "Servidor funcionando!",
